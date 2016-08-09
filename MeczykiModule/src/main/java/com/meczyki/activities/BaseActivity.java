4ce8d6d90ed50e84c38d70c6e2ef5fc2e76ad2f6
AndroidStudio <przemek33665@gmail.com>
2016-08-09 23:00:24 +0200

    comit

--- MeczykiModule/src/main/java/com/meczyki/activities/BaseActivity.java
+++ MeczykiModule/src/main/java/com/meczyki/activities/BaseActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Przemysław Skitał on 2016-07-17.
- */
 public class BaseActivity extends AppCompatActivity {
 
     @Override
